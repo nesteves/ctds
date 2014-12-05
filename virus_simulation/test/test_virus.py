cbcf69b7b54bcad0114e776ce2ebd0a8766f4d9e
Nuno Esteves <nesteves@users.noreply.github.com>
2014-12-05 00:05:19 +0000

    Finish tests for the ResistantVirus class.

--- virus_simulation/test/test_virus.py
+++ virus_simulation/test/test_virus.py
@@ -55,3 +55,62 @@ class SimpleVirusTestCase(unittest.TestCase):
         # Test a value of population density that further reduces the reproduction probability
         mock_random.return_value = 0.25
         self.assertRaises(NoChildException, self.test_virus.reproduce, 0.5)
+
+
+class ResistantVirusTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.birth_prob = 0.5
+        self.clear_prob = 0.5
+        self.resistances = {'drug1': True, 'drug2': False, 'drug3': True}
+        self.mut_prob = 0.2
+        self.test_virus = ResistantVirus(self.birth_prob, self.clear_prob,
+                                         self.resistances, self.mut_prob)
+
+    def test_get_resistances(self):
+        self.assertEquals(self.test_virus.get_resistances(), self.resistances)
+
+    def test_get_mut_prob(self):
+        self.assertEquals(self.test_virus.get_mut_prob(), self.mut_prob)
+
+    def test_is_resistant_to(self):
+        self.assertTrue(self.test_virus.get_resistances()['drug1'],
+                        'The virus should be resistant to "drug1"')
+        self.assertFalse(self.test_virus.get_resistances()['drug2'],
+                         'The virus should not be resistant to "drug2"')
+
+    @mock.patch('random.random')
+    def test_reproduce_succeeds(self, mock_random):
+
+        # Test the method with 0 population density. Allows for a high random.random() which prevents mutation
+        mock_random.return_value = 0.49
+        child_virus = self.test_virus.reproduce(0, ['drug1', 'drug2'])
+        self.assertEquals(child_virus.get_max_birth_prob(), self.test_virus.get_max_birth_prob(),
+                          'Successful reproduction should result in a new virus with the same max birth probability.')
+        self.assertEquals(child_virus.get_clear_prob(), self.test_virus.get_clear_prob(),
+                          'Successful reproduction should result in a new virus with the same clearing probability.')
+        self.assertEquals(child_virus.get_resistances(), self.test_virus.get_resistances(),
+                          'Successful reproduction with no mutation should result in a new '
+                          'virus with the same resistances.')
+
+        # Test the method with a higher population density. Requires a smaller random.random() which enables mutation
+        mock_random.return_value = 0.19
+        child_virus = self.test_virus.reproduce(0.5, [])
+        self.assertEquals(child_virus.get_max_birth_prob(), self.test_virus.get_max_birth_prob(),
+                          'Successful reproduction should result in a new virus with the same max birth probability.')
+        self.assertEquals(child_virus.get_clear_prob(), self.test_virus.get_clear_prob(),
+                          'Successful reproduction should result in a new virus with the same clearing probability.')
+        self.assertEquals(child_virus.get_resistances(), {k: not res for (k, res) in self.test_virus.get_resistances().items()},
+                          'Successful reproduction with mutation should result in a new '
+                          'virus with the reversed.')
+
+    @mock.patch('random.random')
+    def test_reproduce_fails(self, mock_random):
+
+        # Make the method fail because of the population density
+        mock_random.return_value = 0.26
+        self.assertRaises(NoChildException, self.test_virus.reproduce, 0.5, [])
+
+        # Make the method fail because of the active drugs
+        mock_random.return_value = 0.49
+        self.assertRaises(NoChildException, self.test_virus.reproduce, 0, ['drug2'])
